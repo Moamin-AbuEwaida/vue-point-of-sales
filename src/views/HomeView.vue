6c467270d6678b2c854e0486d429d0d5fcb80557
Moamin <moamin.salamah@gmail.com>
2023-01-17 09:54:04 +0100

    setup the project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app
+    id="inspire"
+    :style="{ background: $vuetify.theme.themes.dark.background }"
+  >
+    <v-container fluid> </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+
+  components: {},
+};
+</script>
