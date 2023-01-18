96df396909ebd34d7dae2d003e11c4b5ad226126
Moamin <moamin.salamah@gmail.com>
2023-01-18 15:22:23 +0100

    adding dist folder

---
+++ js/about.db3657ec.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_point_of_sales"]=self["webpackChunkvue_point_of_sales"]||[]).push([[443],{7750:function(n,s,t){t.r(s),t.d(s,{default:function(){return r}});var u=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,s=n._self._c;return s("div",{staticClass:"about"},[s("h1",[n._v("This is an about page")])])}],a=t(1001),l={},i=(0,a.Z)(l,u,e,!1,null,null,null),r=i.exports}}]);
+//# sourceMappingURL=about.db3657ec.js.map
